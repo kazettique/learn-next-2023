63f862201971d50adcce75ce1b5b57b9c6529720
Woody Chang <kazettique@gmail.com>
2023-08-15 07:49:57 +0800

    update

---
+++ src/app/hello.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function hello({}: Props) {
+  return (
+    <div>hello page from app route!</div>
+  )
+}
---
+++ src/app/world.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function world({}: Props) {
+  return (
+    <div>world page from app route.</div>
+  )
+}
